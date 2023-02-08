7e8f1b7036ba119190eb111fb121a86a415371a0
Arnav Pangasa <arnav.pangasa@gmail.com>
2023-02-08 15:56:54 -0500

    Create functionality for requesting and receiving JSON of article content
    Includes text and image IDs ordered as they should appear in the article

--- backend/server.go
+++ backend/server.go
@@ -4,13 +4,73 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
+	"encoding/json"
 )
+
+type JsonMap map[string]interface{}
+type Slice []interface{}
 
 func default_route(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		fmt.Println("New GET request received.")
-		retrieveArticle()
 		fmt.Fprintf(w, "Success!")
+	} else {
+		fmt.Fprintf(w, "Unsupported request type.")
+	}
+}
+
+func article_retrieval(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Println("New GET request received for article retrieval.")
+
+		w.Header().Set("Content-Type", "application/json")
+
+		id := r.URL.Query().Get("id")
+		article := retrieveArticle(id)
+		
+		re := regexp.MustCompile("<image [\w]+>")
+		//content := re.Split(article.Content, -1)
+		content := article.Content
+
+		imageIndices := re.FindAllStringIndex(content, -1)
+
+		var contentList []map[string]string
+
+		j := 0
+		for i := 0; i < len(imageIndices); i++ {
+			nextImageStart := imageIndices[i][0]
+			nextImageEnd := imageIndices[i][1]
+
+			if nextImageStart > j {
+				text := content[j:nextImageStart]
+
+				contentMap := make(map[string]string)
+				contentMap["contentType"] = "text"
+				contentMap["text"] = text
+				contentList = append(contentList, contentMap)
+			}
+			
+			imageString := content[nextImageStart:nextImageEnd]
+			imageID := strings.Split(strings.Split(imageString, "<image ")[0], ">")[0]
+
+			contentMap := make(map[string]string)
+			contentMap["contentType"] = "img"
+			contentMap["id"] = imageID
+			contentList = append(contentList, contentMap)
+
+			j = nextImageEnd
+		}
+		
+		res := JsonMap {
+			"title": article.Title,
+			"content": contentList,
+		}
+
+		jsonRes, err := json.Marshal(res)
+
+		w.Write(jsonRes)
 	} else {
 		fmt.Fprintf(w, "Unsupported request type.")
 	}
@@ -18,6 +78,7 @@ func default_route(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", default_route)
+	http.HandleFunc("/article", article_retrieval)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
