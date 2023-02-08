1ca651c297372ec1cf76f47e29cc2f64f7d20a5d
Sohil Kollipara <sohil.kollipara@gmail.com>
2023-02-07 21:45:45 -0500

    Home Page

---
+++ frontend/src/Home.js
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom';
+import {useState, useEffect} from 'react'
+
+function Home() {
+    //State
+    const [articles, setArticles] = useState(null);
+    useEffect(() => {
+                fetch('http://localhost:5001/articleList')
+                    .then(response => response.json())
+                    .then(result => setArticles(result.list))
+
+        },[])
+
+    const output = []
+
+    if (articles!= null) {
+        articles.forEach((i,x) => {
+            output.push(
+                <p>
+                    <Link to={{pathname:'/article'}}
+                    state={{articleID: i.articleID}}>
+                        <img src={'http://localhost:5001/image/'+i.imageID} /> {i.title}
+                    </Link>
+                </p>)
+        })
+    }
+    if (articles== null) {
+        return(
+            <>Loading!</>
+        )
+    }
+    else {
+        return (
+            <>
+                Welcome to home page!
+                {output}
+            </>
+        )
+    }
+
+
+}
+export default Home;
